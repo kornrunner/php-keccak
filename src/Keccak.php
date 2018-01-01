6de63a27d0745621a2dd7b32403b2d274331f31d
Greg S <greg@evias.be>
2018-01-01 04:51:14 +0100

    remove php5 incompatible code

--- src/Keccak.php
+++ src/Keccak.php
@@ -3,14 +3,12 @@
 namespace kornrunner;
 
 use Exception;
-use function mb_strlen;
-use function mb_substr;
 
 final class Keccak
 {
-    private const KECCAK_ROUNDS = 24;
-    private const LFSR = 0x01;
-    private const ENCODING = '8bit';
+    const KECCAK_ROUNDS = 24;
+    const LFSR = 0x01;
+    const ENCODING = '8bit';
     private static $keccakf_rotc = [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 2, 14, 27, 41, 56, 8, 25, 43, 62, 18, 39, 61, 20, 44];
     private static $keccakf_piln = [10, 7, 11, 17, 18, 3, 5, 16, 8, 21, 24, 4, 15, 23, 19, 13, 12,2, 20, 14, 22, 9, 6, 1];
     private static $x64 = (PHP_INT_SIZE === 8);
